611eddddeaca9716de956983de7acb84a279f317
gianyang <gylim92@gmail.com>
2014-12-12 20:49:08 +0800

    remove delete file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Webapps::Application.routes.draw do
 	
-  get 'static_pages/gallery'
-
   root 'static_pages#home' 
-
+  
   get 'aboutus' => 'static_pages#aboutus'
 
   get 'product' => 'static_pages#product'
+
+  get 'gallery' => 'static_pages#gallery'
 
   get 'contactus' => 'static_pages#contactus'
 
